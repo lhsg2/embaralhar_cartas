4fe697394f258ce6cae9f35266d8637f03d79365
Luiz Henrique <138501027+lhsg2@users.noreply.github.com>
2024-05-25 12:26:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+naipes = ['ouros','copas','paus','espadas']
+numeros =['Ás',2,3,4,5,6,7,8,9,10,'J','Q','K']
+
+cartas=[]
+
+#For para naipes
+for i in range(4):
+#For para numeros
+  for u in range(13):
+    cartas.append(str(numeros[u])+" de "+(naipes[i]))
+quantidade = int(input("degite a quantidade de cartas:"))
+random.shuffle(cartas)
+for i in range(quantidade):
+ print(cartas[i])
